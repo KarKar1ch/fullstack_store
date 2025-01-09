30f6ec41fccf7d0a0114e197d44be4020c6b7feb
Karkar1ch <karina.rogalevich@email.com>
2025-01-09 12:45:38 +0200

    end of main part

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,6 +1,7 @@
 import Router from 'express' 
 import { typeController } from '../controllers/typeController.js'
+import { checkRole } from '../middleware/checkRoleMiddleware.js'
 export const router = new Router()
 
-router.post('/', typeController.create)
+router.post('/',checkRole('ADMIN'), typeController.create)
 router.get('/', typeController.getAll)
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 import Router from 'express' 
 import { userController } from '../controllers/useController.js'
+import { auth } from '../middleware/authMiddleware.js'
 export const router = new Router()
 
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth',auth, userController.check)
