31017b75b47559e2bf810624e171fd7b009e5638
Karkar1ch <karina.rogalevich@email.com>
2025-01-09 11:36:44 +0200

    Finish controller logic (except authorization)

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,30 +1,68 @@
-import { Device } from "../models/models.js";
+import { Device, DeviceInfo } from "../models/models.js";
 import { ApiError } from "../error/ApiError.js";
-import path from 'path'
-import uuid from 'uuid'
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import { fileURLToPath } from 'url';
+import { title } from "process";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 class DeviceController {
-    async create(req, res) {
+    async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-    
-            const device = await Device.create({name, price, brandId, typeId, img: fileName})
-    
-            return res.json(device)
+            const device = await Device.create({name, price, brandId, typeId, img: fileName});
+
+            if (info) {
+                info = JSON.parse(info);
+                info.forEach( i =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                )
+            }
+
+            return res.json(device);
         } catch (e) {
-            next(ApiError.badRequest(e.message))
+            next(ApiError.badRequest(e.message));
         }
     }
 
     async getAll(req, res) {
-        
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let device;
+        if (!brandId & !typeId) {
+            device = await Device.findAndCountAll({ limit, offset})
+        };
+        if (brandId & !typeId) {
+            device = await Device.findAndCountAll({where:{brandId}, limit, offset})
+        }
+        if (!brandId & typeId) {
+            device = await Device.findAndCountAll({where:{typeId}, limit, offset})
+        }
+        if (brandId & typeId) {
+            device = await Device.findAndCountAll({where:{brandId, brandId}, limit, offset})
+        }
+        return res.json(device)
     }
 
     async getOne(req, res) {
-        
+        const {id} = req.params
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            }
+        )
+        return res.json(device)
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,35 +1,41 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import  sequelize  from './bg.js'
-import {User, Basket, BasketDevice, Device, Type, Brand, Rating, DeviceInfo, TypeBrand} from './models/models.js'
-import cors from 'cors'
-import fileUpload from 'express-fileupload'
-import {router as router} from './routes/index.js'
-import { ErrorHandler } from './middleware/ErrorHandlingMiddleware.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import sequelize from './bg.js';
+import { User, Basket, BasketDevice, Device, Type, Brand, Rating, DeviceInfo, TypeBrand } from './models/models.js';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import { router as router } from './routes/index.js';
+import { ErrorHandler } from './middleware/ErrorHandlingMiddleware.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config()
 
-const PORT = process.env.PORT || 5000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(fileUpload({}))
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
-app.use(ErrorHandler)
+app.use(ErrorHandler);
 
-const start = async() => {
-    try{
-        await sequelize.sync()
-        await sequelize.authenticate()
-        app.listen(PORT, ()=>{
-            console.log(`Server starting on port:${PORT}`)
-        })
-    } catch(e) {
-        console.log(e)
+const start = async () => {
+    try {
+        await sequelize.sync();
+        await sequelize.authenticate();
+        app.listen(PORT, () => {
+            console.log(`Server starting on port: ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
     }
-}
+};
 
-
-start()
+start();
