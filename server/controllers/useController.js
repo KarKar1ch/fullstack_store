00e221390466f4a872c5e6b8ecf2cd509496dc88
Karkar1ch <karina.rogalevich@email.com>
2025-01-09 12:23:22 +0200

    finish user controller logic

--- server/controllers/useController.js
+++ server/controllers/useController.js
@@ -1,12 +1,46 @@
 import { ApiError } from "../error/ApiError.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import {User, Basket} from '../models/models.js'
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 
 class UserController {
     async registration(req, res) {
-
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или пароль!'))
+        }
+        const candidate = await User.findOne({where:{email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким email уже cуществует!'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password: hashPassword});
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
-    async login(req, res) {
-        
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async check(req, res, next) {
